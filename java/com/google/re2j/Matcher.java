b9f18698d8d3b99ca6c692e9af1324f9bb3fbbd2
James Ring <sjr@google.com>
2015-02-17 21:34:55 -1000

    fix javadoc-plugin errors

--- java/com/google/re2j/Matcher.java
+++ java/com/google/re2j/Matcher.java
@@ -26,7 +26,7 @@ package com.google.re2j;
  *
  * </ol>
  *
- * <p>See the <href='package-summary.html'>package-level
+ * <p>See the <a href="package-summary.html">package-level
  * documentation</a> for an overview of how to use this API.</p>
  *
  * @author rsc@google.com (Russ Cox)

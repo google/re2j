4db9e9da731a41c187256e8e7f5cf179eefd3f0a
James Ring <sjr@google.com>
2015-06-02 23:04:27 -0700

    port benchmarks to caliper

--- javatests/com/google/re2j/Benchmarks.java
+++ javatests/com/google/re2j/Benchmarks.java
@@ -10,49 +10,38 @@
 
 package com.google.re2j;
 
-import static org.junit.Assert.fail;
+import com.google.caliper.BeforeExperiment;
+import com.google.caliper.Benchmark;
+import com.google.caliper.Param;
+import com.google.caliper.api.VmOptions;
 
 import java.util.regex.Pattern;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TestRule;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
+import static org.junit.Assert.fail;
 
-import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
-import com.carrotsearch.junitbenchmarks.BenchmarkRule;
-
-@RunWith(Parameterized.class)
+/**
+ * Benchmarks for common RE2J operations. The easiest way to run these benchmarks is probably to do:
+ *
+ * <pre>
+ *   mvn exec:java -Dexec.mainClass=com.google.caliper.runner.CaliperMain com.google.re2j.Benchmarks
+ * </pre>
+ */
+@VmOptions("-XX:-TieredCompilation") // http://stackoverflow.com/questions/29199509
 public class Benchmarks {
-
-  @Rule
-  public TestRule benchmarkRule = new BenchmarkRule();
-
-  private final boolean useJdk;
+  @Param({"false", "true"})
+  private boolean useJdk;
 
   private Matcher pathologicalBacktracking;
   private RE2 literal;
   private RE2 notLiteral;
 
-  public Benchmarks(Object useJdk) {
-    this.useJdk = (Boolean) useJdk;
-  }
-
-  @Parameters(name = "useJdk={0}")
-  public static Object[] getParameters() {
-    return new Object[] { false, true };
-  }
-
   private interface Matcher {
     boolean match(String input);
   }
 
-  @Before
+  @BeforeExperiment
   public void setupExpressions() {
-    pathologicalBacktracking = compile("a?a?a?a?a?a?a?a?a?a?a?a?" + "a?a?a?a?a?a?a?a?a?a?a?a?"
+    pathologicalBacktracking = compile("a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?a?"
         + "aaaaaaaaaaaaaaaaaaaaaaaa");
     literal = RE2.compile("y");
     notLiteral = RE2.compile(".y");
@@ -80,8 +69,7 @@ public class Benchmarks {
   }
 
   // See http://swtch.com/~rsc/regexp/regexp1.html.
-  @Test
-  @BenchmarkOptions(benchmarkRounds = 100, warmupRounds = 10)
+  @Benchmark
   public void benchmarkPathologicalBacktracking() {
     if (!pathologicalBacktracking.match("aaaaaaaaaaaaaaaaaaaaaaaa")) {
       fail("no match!");
@@ -91,62 +79,68 @@ public class Benchmarks {
   // The following benchmarks were ported from
   // http://code.google.com/p/go/source/browse/src/pkg/regexp/all_test.go
 
-  @Test
-  @BenchmarkOptions(benchmarkRounds = 10000, warmupRounds = 1500)
-  public void benchmarkLiteral() {
+  @Benchmark
+  public void benchmarkLiteral(int nreps) {
     String x = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxy";
-    if (!literal.match(x)) {
-      fail("no match!");
+    for (int i = 0; i < nreps; i++) {
+      if (!literal.match(x)) {
+        fail("no match!");
+      }
     }
   }
 
-  @Test
-  @BenchmarkOptions(benchmarkRounds = 10000, warmupRounds = 1500)
-  public void benchmarkNotLiteral() {
+  @Benchmark
+  public void benchmarkNotLiteral(int nreps) {
     String x = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxy";
-    if (!notLiteral.match(x)) {
-      fail("no match!");
+    for (int i = 0; i < nreps; i++) {
+      if (!notLiteral.match(x)) {
+        fail("no match!");
+      }
     }
   }
 
-  @Test
-  @BenchmarkOptions(benchmarkRounds = 10000, warmupRounds = 1500)
-  public void benchmarkMatchClass() {
+  @Benchmark
+  public void benchmarkMatchClass(int nreps) {
     String x = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
         + "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" + "w";
     RE2 re = RE2.compile("[abcdw]");
-    if (!re.match(x)) {
-      fail("no match!");
+    for (int i = 0; i < nreps; i++) {
+      if (!re.match(x)) {
+        fail("no match!");
+      }
     }
   }
 
-  @Test
-  @BenchmarkOptions(benchmarkRounds = 10000, warmupRounds = 1500)
-  public void benchmarkMatchClass_InRange() {
+  @Benchmark
+  public void benchmarkMatchClass_InRange(int nreps) {
     // 'b' is between 'a' and 'c', so the charclass
     // range checking is no help here.
     String x = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
         + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" + "c";
     RE2 re = RE2.compile("[ac]");
-    if (!re.match(x)) {
-      fail("no match!");
+    for (int i = 0; i < nreps; i++) {
+      if (!re.match(x)) {
+        fail("no match!");
+      }
     }
   }
 
-  @Test
-  @BenchmarkOptions(benchmarkRounds = 10000, warmupRounds = 1500)
-  public void benchmarkReplaceAll() {
+  @Benchmark
+  public void benchmarkReplaceAll(int nreps) {
     String x = "abcdefghijklmnopqrstuvwxyz";
     RE2 re = RE2.compile("[cjrw]");
-    re.replaceAll(x, "");
+    for (int i = 0; i < nreps; i++) {
+      re.replaceAll(x, "");
+    }
   }
 
-  @Test
-  @BenchmarkOptions(benchmarkRounds = 10000, warmupRounds = 1500)
-  public void benchmarkAnchoredLiteralShortNonMatch() {
+  @Benchmark
+  public void benchmarkAnchoredLiteralShortNonMatch(int nreps) {
     String x = "abcdefghijklmnopqrstuvwxyz";
     RE2 re = RE2.compile("^zbc(d|e)");
-    re.match(x);
+    for (int i = 0; i < nreps; i++) {
+      re.match(x);
+    }
   }
 
   private static final String LONG_DATA;
@@ -159,25 +153,28 @@ public class Benchmarks {
     LONG_DATA = sb.toString();
   }
 
-  @Test
-  @BenchmarkOptions(benchmarkRounds = 10000, warmupRounds = 1500)
-  public void benchmarkAnchoredLiteralLongNonMatch() {
+  @Benchmark
+  public void benchmarkAnchoredLiteralLongNonMatch(int nreps) {
     RE2 re = RE2.compile("^zbc(d|e)");
-    re.match(LONG_DATA);
+    for (int i = 0; i < nreps; i++) {
+      re.match(LONG_DATA);
+    }
   }
 
-  @Test
-  @BenchmarkOptions(benchmarkRounds = 10000, warmupRounds = 1500)
-  public void benchmarkAnchoredShortMatch() {
+  @Benchmark
+  public void benchmarkAnchoredShortMatch(int nreps) {
     String x = "abcdefghijklmnopqrstuvwxyz";
     RE2 re = RE2.compile("^.bc(d|e)");
-    re.match(x);
+    for (int i = 0; i < nreps; i++) {
+      re.match(x);
+    }
   }
 
-  @Test
-  @BenchmarkOptions(benchmarkRounds = 10000, warmupRounds = 1500)
-  public void benchmarkAnchoredLongMatch() {
+  @Benchmark
+  public void benchmarkAnchoredLongMatch(int nreps) {
     RE2 re = RE2.compile("^.bc(d|e)");
-    re.match(LONG_DATA);
+    for (int i = 0; i < nreps; i++) {
+      re.match(LONG_DATA);
+    }
   }
 }

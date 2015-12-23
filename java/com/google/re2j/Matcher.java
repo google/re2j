a30b2ddc0ee59fece3b84cede776042a0dbe05c6
Karol Sobczak <Karol.Sobczak@Teradata.com>
2015-12-23 09:23:40 +0100

    Extract Slice related methods from Presto to separate class with Apache license

--- java/com/google/re2j/Matcher.java
+++ java/com/google/re2j/Matcher.java
@@ -343,77 +343,7 @@ public final class Matcher {
       so.writeBytes(input, appendPos, s - appendPos);
     }
     appendPos = e;
-    int idx = 0;
-
-    // Handle the following items:
-    // 1. ${name};
-    // 2. $0, $1, $123 (group 123, if exists; or group 12, if exists; or group 1);
-    // 3. \\, \$, \t (literal 't').
-    // 4. Anything that doesn't starts with \ or $ is considered regular bytes
-    while (idx < replacement.length()) {
-      byte nextByte = replacement.getByte(idx);
-      if (nextByte == '$') {
-        idx++;
-        if (idx == replacement.length()) {
-          throw new IllegalArgumentException("Illegal replacement sequence: " + replacement.toStringUtf8());
-        }
-        nextByte = replacement.getByte(idx);
-        int backref;
-        if (nextByte == '{') { // case 1 in the above comment
-          idx++;
-          int startCursor = idx;
-          while (idx < replacement.length()) {
-            nextByte = replacement.getByte(idx);
-            if (nextByte == '}') {
-              break;
-            }
-            idx++;
-          }
-          String groupName = replacement.slice(startCursor, idx - startCursor).toStringUtf8();
-          Integer namedGroupIndex = pattern.re2().namedGroupIndexes.get(groupName);
-          if (namedGroupIndex == null) {
-            throw new IndexOutOfBoundsException("Illegal replacement sequence: unknown group " + groupName);
-          }
-          backref = namedGroupIndex;
-          idx++;
-        } else { // case 2 in the above comment
-          backref = nextByte - '0';
-          if (backref < 0 || backref > 9) {
-            throw new IllegalArgumentException("Illegal replacement sequence: " + replacement.toStringUtf8());
-          }
-          if (groupCount < backref) {
-            throw new IndexOutOfBoundsException("Illegal replacement sequence: unknown group " + backref);
-          }
-          idx++;
-          while (idx < replacement.length()) { // Adaptive group number: find largest group num that is not greater than actual number of groups
-            int nextDigit = replacement.getByte(idx) - '0';
-            if (nextDigit < 0 || nextDigit > 9) {
-              break;
-            }
-            int newBackref = (backref * 10) + nextDigit;
-            if (groupCount < newBackref) {
-              break;
-            }
-            backref = newBackref;
-            idx++;
-          }
-        }
-        Slice group = group(backref);
-        if (group != null) {
-          so.writeBytes(group);
-        }
-      } else { // case 3 and 4 in the above comment
-        if (nextByte == '\\') {
-          idx++;
-          if (idx == replacement.length()) {
-            throw new IllegalArgumentException("Illegal replacement sequence: " + replacement.toStringUtf8());
-          }
-          nextByte = replacement.getByte(idx);
-        }
-        so.appendByte(nextByte);
-        idx++;
-      }
-    }
+    SliceUtils.appendReplacement(so, replacement, this);
     return this;
   }
 

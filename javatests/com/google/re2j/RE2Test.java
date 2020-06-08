a5966afb0f6626a816a316085e3c35d9b2ad1e79
James Ring <sjr@jdns.org>
2020-06-08 14:34:42 -0700

    replace calls to assertEquals(true/false, ...) to assertTrue/assertFalse(...)

--- javatests/com/google/re2j/RE2Test.java
+++ javatests/com/google/re2j/RE2Test.java
@@ -7,26 +7,27 @@
 
 package com.google.re2j;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /** Tests of RE2 API. */
 public class RE2Test {
   @Test
   public void testFullMatch() {
-    assertEquals(true, new RE2("ab+c").match("abbbbbc", 0, 7, RE2.ANCHOR_BOTH, null, 0));
-    assertEquals(false, new RE2("ab+c").match("xabbbbbc", 0, 8, RE2.ANCHOR_BOTH, null, 0));
+    assertTrue(new RE2("ab+c").match("abbbbbc", 0, 7, RE2.ANCHOR_BOTH, null, 0));
+    assertFalse(new RE2("ab+c").match("xabbbbbc", 0, 8, RE2.ANCHOR_BOTH, null, 0));
   }
 
   @Test
   public void testFindEnd() {
     RE2 r = new RE2("abc.*def");
-    assertEquals(true, r.match("yyyabcxxxdefzzz", 0, 15, RE2.UNANCHORED, null, 0));
-    assertEquals(true, r.match("yyyabcxxxdefzzz", 0, 12, RE2.UNANCHORED, null, 0));
-    assertEquals(true, r.match("yyyabcxxxdefzzz", 3, 15, RE2.UNANCHORED, null, 0));
-    assertEquals(true, r.match("yyyabcxxxdefzzz", 3, 12, RE2.UNANCHORED, null, 0));
-    assertEquals(false, r.match("yyyabcxxxdefzzz", 4, 12, RE2.UNANCHORED, null, 0));
-    assertEquals(false, r.match("yyyabcxxxdefzzz", 3, 11, RE2.UNANCHORED, null, 0));
+    assertTrue(r.match("yyyabcxxxdefzzz", 0, 15, RE2.UNANCHORED, null, 0));
+    assertTrue(r.match("yyyabcxxxdefzzz", 0, 12, RE2.UNANCHORED, null, 0));
+    assertTrue(r.match("yyyabcxxxdefzzz", 3, 15, RE2.UNANCHORED, null, 0));
+    assertTrue(r.match("yyyabcxxxdefzzz", 3, 12, RE2.UNANCHORED, null, 0));
+    assertFalse(r.match("yyyabcxxxdefzzz", 4, 12, RE2.UNANCHORED, null, 0));
+    assertFalse(r.match("yyyabcxxxdefzzz", 3, 11, RE2.UNANCHORED, null, 0));
   }
 }
